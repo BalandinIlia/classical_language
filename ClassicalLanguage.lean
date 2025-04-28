ca6b81c813f9262dd2dabbbcefc65077bc4e9bbc
BalandinIlia <ilia.balandin@student.uva.nl>
2025-04-28 15:36:09 +0200

    Update ClassicalLanguage.lean

--- ClassicalLanguage.lean
+++ ClassicalLanguage.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `ClassicalLanguage` library.
 -- Import modules here that should be built as part of the library.
-import ClassicalLanguage.Basic
+import ClassicalLanguage.de_definitions
