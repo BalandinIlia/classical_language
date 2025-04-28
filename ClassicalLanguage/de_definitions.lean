d2ed0601eacf922f79c183908a7978659f14d321
BalandinIlia <ilia.balandin@student.uva.nl>
2025-04-28 16:23:36 +0200

    Update de_definitions.lean

--- ClassicalLanguage/de_definitions.lean
+++ ClassicalLanguage/de_definitions.lean
@@ -319,3 +319,56 @@ theorem terminates(prog: Program):
 
   | whilee c body ih =>
     simp [noLoop]
+
+-- HOARE LOGIC
+
+def hoare(P: Cond)(prog: Program)(Q: Cond): Prop := 
+    ∀sStart sFin: State, evalC P sStart → BSOS sStart prog sFin → evalC Q sFin
+
+theorem hoareSeq(P R Q: Cond)(p1 p2: Program):
+  ((hoare P p1 R) → (hoare R p2 Q) → (hoare P (Program.seq p1 p2) Q)) := by
+  intro hoare1
+  intro hoare2
+
+  rw [hoare]
+  intro sStart
+  intro sFin
+  intro preCond
+  intro trans
+
+  cases trans
+  case seq sInter trans1 trans2 =>
+    apply hoare2 sInter sFin
+    apply hoare1 sStart sInter
+    apply preCond
+    apply trans1
+    apply trans2
+
+theorem hoareAssign(name: String)(expr: Expr)(Q: Cond):
+  hoare (replC Q name expr) (Program.assign name expr) Q := by
+  rw [hoare]
+  intro sStart sFin
+  rw [condReplacement]
+  intro precond
+  intro trans
+  cases trans
+  case assign =>
+    apply precond
+
+theorem hoareIf(P Q: Cond)(cond: Cond)(pt pf: Program):
+  hoare P pt Q → hoare P pf Q → hoare P (Program.iff cond pt pf) Q := by
+  intro hoaret
+  intro hoaref
+  rw [hoare]
+  intro sStart sFin
+  intro preCond
+  intro trans
+  cases trans
+  case if_true condVal transt =>
+    apply hoaret sStart
+    apply preCond
+    apply transt
+  case if_false condVal transf =>
+    apply hoaref sStart
+    apply preCond
+    apply transf
