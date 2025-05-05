d7724433db8bd67736c5523ec2af16fcf1faf767
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-05-05 12:16:32 +0200

    refactoring

--- ClassicalLanguage.lean
+++ ClassicalLanguage.lean
@@ -1,9 +1,12 @@
 -- This module serves as the root of the `ClassicalLanguage` library.
 -- Import modules here that should be built as part of the library.
+
+import ClassicalLanguage.State.State
 import ClassicalLanguage.DeepEmbedding.de_basic
 import ClassicalLanguage.DeepEmbedding.de_BSOS
-import ClassicalLanguage.DeepEmbedding.de_unlooping
-import ClassicalLanguage.DeepEmbedding.de_termination_determenism
 import ClassicalLanguage.DeepEmbedding.de_hoare
-import ClassicalLanguage.DeepEmbedding.de_SSOS
-import ClassicalLanguage.DeepEmbedding.de_SSOS_alternative
+import ClassicalLanguage.DeepEmbedding.de_termination_determenism
+import ClassicalLanguage.ShallowEmbedding.first
+import ClassicalLanguage.Bonus.de_SSOS
+import ClassicalLanguage.Bonus.de_SSOS_alternative
+import ClassicalLanguage.Bonus.de_unlooping
