600f902ac9cd4a55f6d694428150ad3f70c3adad
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-06-10 13:54:03 +0200

    comment

--- ClassicalLanguage/State/State.lean
+++ ClassicalLanguage/State/State.lean
@@ -4,6 +4,6 @@ import Mathlib.Data.Int.Basic
 def State := String → ℤ
 
 -- This function describes a state2 based on state1
--- State2 is the same as state1, but one given name is assigned one given value.
+-- State2 is the same as state1, but one given name is assigned a different value.
 def replS: State → String → ℤ → State
 | st, name, val => (fun s:String => if(s==name) then val else st s)
